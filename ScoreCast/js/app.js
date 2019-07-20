a6aeb76822697dc0c0f4ca64854ef2ac418632b8
Katelyn <katelyncmiles@gmail.com>
2019-07-19 20:34:46 -0400

    Draft

--- ScoreCast/js/app.js
+++ ScoreCast/js/app.js
@@ -1,23 +1,23 @@
 console.log('working?');
 
 $(() => {
+  //Click event when you press enter button
   $('form').on('click', `#enterWeather`, event => {
     event.preventDefault();
     let $inputBox = $('#input-box');
     let $zipCode = $inputBox.val();
+
+    //Endpoint for OpenWeather API
     const endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${$zipCode},us&units=imperial&appid=54358d797d854fe643545e22f50f01c5`;
     $.ajax({ url: endpoint }).then(handleData);
 
     $inputBox.val('');
     $('h2').remove();
     $('img').remove();
-    // console.log(endpoint);
-    // console.log('clicked');
-    // console.log($zipCode);
   });
 
+  //Handle Data function for OpenWeather API
   const handleData = data => {
-    console.log(data);
     const $townName = $('<h2>').attr('id', 'town');
     $townName.text(data.name);
     $('#left').append($townName);
@@ -30,10 +30,10 @@ $(() => {
     $temp.text(`Temperature is ${data.main.temp}°F`);
     $('#left').append($temp);
 
+    //Conditional for photos corresponding to weather keyword
     if (data.weather[0].main === 'Clear') {
       let $clear = $(
         '<img src="https://cdn.pixabay.com/photo/2014/03/27/23/57/blue-sky-299765_1280.jpg" alt="Sunny clear sky">'
-        //https://media1.thehungryjpeg.com/thumbs/800_3549446_ksv2wy8k5t06sv4je2vyjmsbqhaoz2eod9hk5ejg.jpg" alt="Sunny clear sky
       );
       $($main.before($clear));
     } else if (data.weather[0].main === 'Rain') {
@@ -69,54 +69,28 @@ $(() => {
     } else if (data.weather[0].main === 'Drizzle') {
       let $clear = $(
         '<img src="https://images.unsplash.com/photo-1417008914239-59b898b49382?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1664&q=80" alt="Drizzle rain">'
-        //https://media1.thehungryjpeg.com/thumbs/800_3549446_ksv2wy8k5t06sv4je2vyjmsbqhaoz2eod9hk5ejg.jpg" alt="Sunny clear sky
       );
       $($main.before($clear));
     }
 
+    //Endpoint for last.fm API
     const endpoint2 = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${
       data.weather[0].main
     }&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json`;
     $.ajax({ url: endpoint2 }).then(handleData2);
 
+    //reset screen when typing in a new zip code
     $('h3').remove();
     $('h4').remove();
     $('a').remove();
-
-    // const endpoint3 = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${}&key=AIzaSyCB_CT_KePzmlr-VWqMCuzlb09nDK2x904`
   };
 
-  //   $('form').on('click', `#enterMusic`, event => {
-  //     event.preventDefault();
-  //     let $inputBox2 = $('#input-box2');
-  //     let $tag = $inputBox2.val();
-  //     // const endpoint = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${$tag}&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json`;
-  //     const endpoint2 = `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${$tag}&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json`;
-  //     $.ajax({ url: endpoint2 }).then(handleData2);
-  //     $inputBox2.val('');
-  //     $('h3').remove(); ///make it so it doesn't remove weather h2
-  //     console.log(endpoint);
-  //     console.log('clicked');
-  //     // console.log($tag);
-  //   });
-
+  //Handle Data function for last.fm API
   const handleData2 = data => {
-    console.log(data);
-    // const $name = $('<h3>');
-    // $name.text(data.tracks.track[0].name);
-    // $('body').append($name);
-
-    // const $trackLink = $('<h3>');
-    // $trackLink.text(data.tracks.track[0].url);
-    // $('body').append($trackLink);
-
+    //Append data to screen
     const $songToday = $('<h3>');
     $songToday.text("Today's Tune:").attr('id', 'songToday');
     $('#right').append($songToday);
-
-    // const $songName = $('<h3>');
-    // $songName.text(data.results.trackmatches.track[0].name);
-    // $('body').append($songName);
 
     const $songObject =
       data.results.trackmatches.track[
@@ -131,68 +105,20 @@ $(() => {
     $artist.text('by ' + $songObject.artist);
     $('#right').append($artist);
 
-    // const $trackLink = $('<a>');
-    // $trackLink.attr('href', $songObject.url);
-    // $trackLink.text('Go to track!');
-    // // $trackLink.text(data.results.trackmatches.track[0].url);
-    // $('body').append($trackLink);
-
+    //Endpoint for YouTube API
     const endpoint3 = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${
       $songObject.name
     }${$songObject.artist}&key=AIzaSyCB_CT_KePzmlr-VWqMCuzlb09nDK2x904`;
 
     $.ajax({ url: endpoint3 }).then(handleData3);
-    console.log(endpoint3);
-    console.log($songName);
 
+    //reset video when new zip code is entered
     $('iframe').remove();
-
-    // const $songName = $('<h3>');
-    // $songName.text(
-    //   data.results.trackmatches.track[
-    //     Math.floor(Math.random() * data.results.trackmatches.track.length)
-    //   ].name
-    // );
-    // $('body').append($songName);
-
-    // const $artist = $('<h3>');
-    // $artist.text('by ' + data.results.trackmatches.track[0].artist);
-    // $('body').append($artist);
-
-    // const $artist = $('<h3>');
-    // $artist.text(
-    //   'by ' +
-    //     data.results.trackmatches.track[
-    //       Math.floor(Math.random() * data.results.trackmatches.track.length)
-    //     ].artist
-    // );
-    // $('body').append($artist);
-
-    // const $trackLink = $('<a>');
-    // $trackLink.attr('href', data.results.trackmatches.track[0].url);
-    // $trackLink.text('Go to track!');
-    // // $trackLink.text(data.results.trackmatches.track[0].url);
-    // $('body').append($trackLink);
-
-    // const $trackLink = $('<a>');
-    // $trackLink.attr(
-    //   'href',
-    //   data.results.trackmatches.track[
-    //     Math.floor(Math.random() * data.results.trackmatches.track.length)
-    //   ].url
-    // );
-    // $trackLink.text('Go to track!');
-    // // $trackLink.text(data.results.trackmatches.track[0].url);
-    // $('body').append($trackLink);
-
-    // const $trackEmbed =
   };
 
+  //Handle Data function for YouTube API
   const handleData3 = data => {
-    console.log(data);
-    console.log(data.items[0].id.videoId);
     const $embedVideo = $('<iframe>', {
-      //   width: 80,
       height: 315,
       src: `https://www.youtube.com/embed/${data.items[0].id.videoId}`,
       frameborder: 0,
@@ -202,20 +128,7 @@ $(() => {
     $('#right').append($embedVideo);
   };
 
-  //   $('form').on('click', `#enterVid`, event => {
-  //     event.preventDefault();
-  //     let $inputBox2 = $('#input-box3');
-  //     let $tag = $inputBox3.val();
-  //     // const endpoint = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${$tag}&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json`;
-  //     const endpoint2 = `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${$tag}&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json`;
-  //     $.ajax({ url: endpoint2 }).then(handleData2);
-  //     $inputBox2.val('');
-  //     $('h3').remove(); ///make it so it doesn't remove weather h2
-  //     console.log(endpoint);
-  //     console.log('clicked');
-  //     // console.log($tag);
-  //   });
-
+  //"About" modal for the page
   const $modal = $('#modal');
   const $closeBtn = $('#close');
 
@@ -229,57 +142,3 @@ $(() => {
   setTimeout(openModal, 1000);
   $closeBtn.on('click', closeModal);
 });
-
-//   let $inputBox = $('#input-box');
-//   let $zipCode = $inputBox.val();
-//   let endpoint = `http://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=54358d797d854fe643545e22f50f01c5`;
-
-//   const handleData = data => {
-//     console.log(data);
-//   };
-
-//   $.ajax({
-//     url: endpoint
-//   }).then(handleData);
-
-// api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=54358d797d854fe643545e22f50f01c5
-
-// weather.main: Clear, Rain, Haze, Clouds, Thunderstorm
-
-// https://media4.s-nbcnews.com/i/newscms/2018_16/1333661/lightening-stock-today-180420-tease_4aeb5d5a1bdfb4d9b15ba54730fbb086.jpg
-
-// http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=disco&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json
-
-// try adding:
-// const endpoint2 = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${$tag}&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json`;
-//     $.ajax({ url: endpoint2 }).then(handleData2);
-
-// const endpoint2 = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=disco&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json`;
-//     $.ajax({ url: endpoint2 }).then(handleData2);
-
-//using disco as place holder right now but can eventually use some sort of variable/array to loop through for tag name, or search song by something else
-
-//maybe randomize the index that it's calling to get a different song every time
-
-// http://ws.audioscrobbler.com/2.0/?method=track.search&track=Believe&api_key=241e400dcad5f275ae171eefc9ad9b3d&format=json
-
-//plan A: Attempt to embed lastfm link
-//plan B: use different Music API where I Can embed the audio
-//plan C: link to third API using search term returned from lastfm where I can embed the audio
-
-// yt_search(term = NULL, max_results = 50, channel_id = NULL,
-//     channel_type = NULL, type = "video", event_type = NULL,
-//     location = NULL, location_radius = NULL, published_after = NULL,
-//     published_before = NULL, video_definition = "any",
-//     video_caption = "any", video_license = "any", video_syndicated = "any",
-//     video_type = "any", simplify = TRUE, get_all = TRUE,
-//     page_token = NULL, ...)
-
-//Youtube API key: AIzaSyCB_CT_KePzmlr-VWqMCuzlb09nDK2x904
-
-//https://www.googleapis.com/youtube/v3/search?part=snippet&q=surfing&key=[YOUR_API_KEY]
-
-//https://cdn.pixabay.com/photo/2014/03/27/23/57/blue-sky-299765_1280.jpg
-
-///ADD snow condition
-//sleet?
